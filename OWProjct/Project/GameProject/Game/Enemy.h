7f07592d06ca283b3688512b155831b4da0d8f73
camuchasu <24051908@d.anabuki-college.net>
2025-09-08 11:57:16 +0900

    Base切り替え

--- OWProjct/Project/GameProject/Game/Enemy.h
+++ OWProjct/Project/GameProject/Game/Enemy.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "Base/Base.h"
+class  Enemy : public Base
+{
+public:
+	CImage m_img;
+private:
+	Enemy(const CVector2D& pos);
+	~Enemy();
+	void Update();
+	void Draw();
+	void Collision();
+};
