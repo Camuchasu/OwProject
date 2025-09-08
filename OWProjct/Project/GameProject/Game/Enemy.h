6f9d8e48a0c68b4ec3b116a7f946412e98710be3
camuchasu <24051908@d.anabuki-college.net>
2025-09-08 17:27:10 +0900

    Enemyの変更

--- OWProjct/Project/GameProject/Game/Enemy.h
+++ OWProjct/Project/GameProject/Game/Enemy.h
@@ -3,11 +3,12 @@
 class Enemy : public Base
 {
 public:
-	CImage m_img;
-private:
-	Enemy(const CVector2D& pos,bool flip);
+	
+	Enemy(const CVector2D& pos, bool flip);
 	~Enemy();
 	void Update()override;
 	void Draw()override;
 	void Collision(Base* b);
+private:
+	CImage m_img;
 };
