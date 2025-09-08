412be2c42f457da0cecec7dcc7c533c5f575b396
yuutooo32 <yourentuju@gmail.com>
2025-09-08 15:12:20 +0900

    playerって感じ

--- OWProjct/Project/GameProject/Game/Player.cpp
+++ OWProjct/Project/GameProject/Game/Player.cpp
@@ -1,7 +1,78 @@
 #include "Player.h"
 
-Player::Player(const CVector2D& pos, float ang) :Base(eType_Player) {
+void Player::StateIdle(){
+	//移動量
+	const float move_speed = 6;
+	//移動フラグ
+	bool move_flag = false;
+	//ジャンプ力
+	const float jump_pow = 12;
+	//左移動
+	if (HOLD(CInput::eLeft)) {
+		//移動量を設定
+		m_pos.x += -move_speed;
+		//反転フラグ
+		m_flip = true;
+		move_flag = true;
+	}
+	//右移動
+	if (HOLD(CInput::eRight)) {
+		//移動量を設定
+		m_pos.x += move_speed;
+		//反転フラグ
+		m_flip = false;
+		move_flag = true;
+	}
+	//ジャンプ
+	if (m_is_ground && PUSH(CInput::eButton2)) {
+		m_vec.y = -jump_pow;
+		m_is_ground = false;
+	}
+	//ジャンプ中なら
+	if (!m_is_ground) {
+		if (m_vec.y < 0)
+			//上昇アニメーション
+			m_img.ChangeAnimation(eAnimJumpUp, false);
+		else
+			//下降アニメーション
+			m_img.ChangeAnimation(eAnimJumpDown, false);
+	}
+	//地面にいるなら
+	else
+	{
+		//移動中なら
+		if (move_flag) {
+			//走るアニメーション
+			m_img.ChangeAnimation(eAnimRun);
+		}
+		else {
+			//待機アニメーション
+			m_img.ChangeAnimation(eAnimIdle);
+		}
+	}
+}
 
+void Player::StateDamage(){
+	m_img.ChangeAnimation(eAnimDamage, false);
+	if (m_img.CheckAnimationEnd()) {
+		m_state = eState_Idle;
+	}
+}
+
+void Player::StateDown(){
+	m_img.ChangeAnimation(eAnimDown, false);
+	if (m_img.CheckAnimationEnd()) {
+		SetKill();
+	}
+}
+
+Player::Player(const CVector2D& pos) :Base(eType_Player) {
+	m_img = COPY_RESOURCE("Player", CImage);
+	m_pos_old = m_pos = pos;
+	m_rad = 12;
+	m_img.ChangeAnimation(0);
+	m_img.SetSize(32, 32);
+	m_img.SetCenter(16, 16);
 }
 
 Player::~Player(){
@@ -9,11 +80,38 @@ Player::~Player(){
 }
 
 void Player::Update(){
+	m_pos_old = m_pos;
+	switch (m_state) {
+		//通常状態
+	case eState_Idle:
+		StateIdle();
+		break;
+		//ダメージ状態
+	case eState_Damage:
+		StateDamage();
+		break;
+		//ダウン状態
+	case eState_Down:
+		StateDown();
+		break;
+	}
+	//落ちていたら落下中状態へ移行
+	if (m_is_ground && m_vec.y > GRAVITY * 4)
+		m_is_ground = false;
+	//重力による落下
+	m_vec.y += GRAVITY;
+	m_pos += m_vec;
+	m_img.UpdateAnimation();
+	//スクロール設定
+	m_scroll.x = m_pos.x - 1280 / 2;
+	m_scroll.y = m_pos.y - 550 / 2;
 
 }
 
 void Player::Draw(){
-
+	m_img.SetPos(GetScreenPos(m_pos));
+	m_img.SetFlipH(m_flip);
+	m_img.Draw();
 }
 
 void Player::Collision(Base* b){
--- OWProjct/Project/GameProject/Game/Player.h
+++ OWProjct/Project/GameProject/Game/Player.h
@@ -1,11 +1,40 @@
 #pragma once
 #include"Base/Base.h"
 class Player : public Base {
+private:
+	//アニメーションの種類
+	enum {
+		eAnimIdle = 0,
+		eAnimRun,
+		eAnimJumpUp,
+		eAnimJumpDown,
+		eAnimDamage,
+		eAnimDown,
+	};
+	//状態
+	enum {
+		eState_Idle,
+		eState_Damage,
+		eState_Down,
+	};
+	//状態変数
+	int m_state;
+	//各状態での挙動
+	void StateIdle();
+	void StateDamage();
+	void StateDown();
+
 	CImage m_img;
+	bool m_flip;
+	bool m_is_ground;
+
 public:
-	Player(const CVector2D& pos, float ang);
+	Player(const CVector2D& pos);
 	~Player();
 	void Update();
 	void Draw();
 	void Collision(Base* b);
+
+	static TexAnimData _anim_data[];
+
 };
