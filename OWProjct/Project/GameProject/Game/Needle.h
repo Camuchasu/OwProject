08a145a7cef8b138ca3fc7d8a9a8b1157134a537
camuchasu <24051908@d.anabuki-college.net>
2025-09-11 14:33:07 +0900

    hai

--- OWProjct/Project/GameProject/Game/Needle.h
+++ OWProjct/Project/GameProject/Game/Needle.h
@@ -3,12 +3,21 @@
 
 class Needle :public Base 
 {
+	
 private:
 	CImage m_needle;
-	CImage m_needle2;
 	bool m_isGround;
 	bool flag;
 public:
+	struct TypeNeedle
+	{
+		enum
+		{
+			eNeedle,
+			eFoolNeedle,
+		};
+		
+	};
 	Needle(CVector2D& pos);
 	void Update();
 	void Draw();
