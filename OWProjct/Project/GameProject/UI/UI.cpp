9aab50cd6cdbe70d5ef8bf470b17cb1ea36e5224
camuchasu <24051908@d.anabuki-college.net>
2025-09-12 09:40:20 +0900

    kk

--- OWProjct/Project/GameProject/UI/UI.cpp
+++ OWProjct/Project/GameProject/UI/UI.cpp
@@ -16,7 +16,7 @@ void UI::Update()
 
 void UI::Draw()
 {
-	m_UI1.SetPos(GetScreenPos(CVector2D(m_pos.x + 110, 1080 / 2 - 50)));
+	m_UI1.SetPos(GetScreenPos(CVector2D(m_pos.x + 130, 1080 / 2 - 50)));
 	m_UI1.SetSize(100, 100);
 	m_UI1.Draw();
 
